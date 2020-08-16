bd2522a21cdacb237cced98a36003164a239adad
viveklucky <sanodiyav94@gmail.com>
2020-08-16 23:30:26 +0530

    login logout

--- telusko/urls.py
+++ telusko/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('travello.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns = urlpatterns + \
